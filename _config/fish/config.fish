dedf8592d23cac762905330465193936372fb976
Ueda Daiki <ueda.daiki@teamspirit.com>
2025-10-29 14:54:47 +0900

    JAVA_HOMEの設定をfish/config.localから戻した
    設定箇所が分かりにくいため。またブランチ運用で利用端末ごとの差異を吸収して、config.localの使用は削減したい方針にしたため

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -18,6 +18,8 @@ if test -d $PYENV_ROOT/bin
     pyenv init - fish | source
 end
 
+set -x JAVA_HOME (/usr/libexec/java_home -v 17)
+
 alias rm /opt/homebrew/opt/trash/bin/trash
 
 function display-toggle --description '外部モニターの解像度を 2560x1440 / 3008x1692 でトグル'

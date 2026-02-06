ba4bc787e1bc7373d12fd62e840168f7e5f44902
daikiueda, @ue_di <daikiueda.1979@gmail.com>
2026-02-07 01:22:28 +0900

    Added  zoxide init to config.fish

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -27,6 +27,8 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (/opt/homebrew/bin/brew shellenv)
+    
+    zoxide init fish | source
 
     fish_add_path /opt/homebrew/share/git-core/contrib/diff-highlight/
 
